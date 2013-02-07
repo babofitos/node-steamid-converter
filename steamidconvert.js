85bb63c08f4264dd6ca6a09f295f9b64c949a482
babofitos <christopherchoi@outlook.com>
2013-02-07 18:34:38 -0500

    add steamid64 to steamid method

--- steamidconvert.js
+++ steamidconvert.js
@@ -20,3 +20,35 @@ exports.convertTo64 = function (steamid, callback) {
     }
   }
 }
+
+exports.convertToText = function(steamid64, callback) {
+  var w
+    , x = BigNumber('76561197960265728')
+    , y = 0
+    , sid
+    , out = ['STEAM_0']
+    , err = null
+  if (typeof steamid64 !== 'string') {
+    throw new Error('steamid must be string')
+  }
+  try {
+    w = BigNumber(steamid64)
+    if (w.mod(2).toPrecision(1) === '1') y = 1
+    out.push(y)
+    sid = w.minus(y).minus(x).div(2).toPrecision(17)
+    sid = sid.substring(0, sid.indexOf('.'))
+    if (sid) {
+      out.push(sid)
+    } else {
+      throw new Error('Invalid SteamID64')
+    }
+  }
+  catch (e) {
+    err = e.message
+  }
+  finally {
+    if (typeof callback === 'function') {
+      callback(err, out.join(':'))
+    }
+  }
+}
