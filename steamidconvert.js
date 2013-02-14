53633db2f7abdf7cb3e448d174bd5a68d0c10eb3
babofitos <scvfighting@gmail.com>
2013-02-14 15:59:23 -0500

    better error checking

--- steamidconvert.js
+++ steamidconvert.js
@@ -4,19 +4,42 @@ exports.convertTo64 = function (steamid) {
   var x = BigNumber('76561197960265728')
     , err = null
     , sid
-    , sidSplit = steamid.split(':')
-    , z = sidSplit[2]
-    , y = sidSplit[1]
+    , sidSplit
+    , z
+    , y
+
   try {
-    if (z && y) sid = x.plus(z*2).plus(y).toPrecision(17)
-    else throw "Invalid STEAM_ID"
+    if (!steamid) {
+      throw new ReferenceError('SteamID argument required')
+    } 
+    else if (typeof steamid !== 'string') {
+      throw new TypeError('SteamID must be a string')
+    }
+    else {
+      sidSplit = steamid.split(':')
+      , z = sidSplit[2]
+      , y = sidSplit[1]
+    }
+
+    if (z && y) {
+      sid = x.plus(z*2).plus(y).toPrecision(17)
+    }
+    else {
+      throw new Error('Invalid SteamID')
+    }
   }
+
   catch (e) {
     err = e
   }
+
   finally {
-    if (err) return err
-    else return sid
+    if (err) {
+      throw err
+    }
+    else {
+      return sid
+    }
   }
 }
 
@@ -27,26 +50,41 @@ exports.convertToText = function(steamid64) {
     , sid
     , out = ['STEAM_0']
     , err = null
-  if (typeof steamid64 !== 'string') {
-    throw new Error('steamid must be string')
-  }
+
   try {
+    if (!steamid64) {
+      throw new ReferenceError('SteamID64 argument required')
+    }
+    else if (typeof steamid64 !== 'string') {
+      throw new TypeError('SteamID must be a string')
+    }
+
     w = BigNumber(steamid64)
-    if (w.mod(2).toPrecision(1) === '1') y = 1
+    if (w.mod(2).toPrecision(1) === '1') {
+      y = 1
+    }
     out.push(y)
     sid = w.minus(y).minus(x).div(2).toPrecision(17)
     sid = parseInt(sid, 10)
-    if (sid) {
-      out.push(sid)
-    } else {
+
+    if (sid < 0 || !sid) {
       throw new Error('Invalid SteamID64')
+    } 
+    else {
+      out.push(sid)
     }
   }
+
   catch (e) {
     err = e.message
   }
+
   finally {
-    if (err) return err
-    else return out.join(':')
+    if (err) {
+      throw err
+    }
+    else {
+      return out.join(':')
+    }
   }
 }
